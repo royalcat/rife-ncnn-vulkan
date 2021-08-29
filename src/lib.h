e47136fda2c5e5c8b692f3e957fa8ddd4eca9997
RoyalCat <k.adamovich20@gmail.com>
2021-08-30 00:36:58 +0300

    fix win build again

--- src/lib.h
+++ src/lib.h
@@ -15,25 +15,25 @@
 
 extern "C" {
 struct RifeParameters {
-  const char* model;
+    const char* model;
 
-  const int* gpuids;
-  const int gpucount;
+    const int* gpuids;
+    const int gpucount;
 
-  const int* job_proc;
-  const int job_proc_size;
+    const int* job_proc;
+    const int job_proc_size;
 
-  const int verbose;
-  const int tta_mode;
-  const int uhd_mode;
+    const int verbose;
+    const int tta_mode;
+    const int uhd_mode;
 };
 
-EXPORT static std::vector<RIFE*> rife;
+static std::vector<RIFE*> rife;
 
-EXPORT extern int init(RifeParameters params);
+EXPORT int init(RifeParameters params);
 
-EXPORT extern int process(const unsigned char* pixels0,
-                          const unsigned char* pixels1,
-                          unsigned char* outpixels, int width, int height,
-                          int pixels_type, float timestep, int gpuindex);
+EXPORT int process(const unsigned char* pixels0,
+                   const unsigned char* pixels1,
+                   unsigned char* outpixels, int width, int height,
+                   int pixels_type, float timestep, int gpuindex);
 }
